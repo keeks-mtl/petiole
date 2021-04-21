2f00db4d08623c08ea09b7e8d658c8de1d2517fc
keeks-mtl <wallarand@gmail.com>
2021-04-21 16:44:34 +0000

    Change styling on register.html

--- app.py
+++ app.py
@@ -37,6 +37,9 @@ def register():
             return redirect(url_for("register"))
 
         register = {
+            "first_name" : request.form.get("first_name").lower(),
+            "last_name" : request.form.get("last_name").lower(),
+            "email" : request.form.get("email"),
             "username": request.form.get("username").lower(),
             "password": generate_password_hash(request.form.get("password"))
         }
