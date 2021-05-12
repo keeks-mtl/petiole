074625259f17d442c435a0ccb2357b7b88ebe5c2
keeks-mtl <wallarand@gmail.com>
2021-05-12 14:13:19 +0000

    Change order of plants on plants.html

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def plants():
     page = int(request.args.get('page') or 1)
     num = 12
     count = ceil(float(plants_collection.count_documents({}) / num))
-    plants = list(plants_collection.find({}).skip((page - 1) * num).limit(num))
+    plants = list(plants_collection.find({}).sort("plant_like", -1).skip((page - 1) * num).limit(num))
     return render_template(
         "plants.html", plants=plants,
         page=page, count=count, search=False)
