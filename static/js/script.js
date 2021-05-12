07c440dd186d918a3dc93ef595ab7dd9fe759123
keeks-mtl <wallarand@gmail.com>
2021-05-12 12:12:07 +0000

    Add delete functionality to modal in js file

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,17 @@
-const button = document.querySelector("#likePlant");
-button.addEventListener('click', function() {
-    button.classList.toggle("fas")
-    button.classList.toggle("far")
-})
+// const button = document.querySelector("#likePlant");
+// button.addEventListener('click', function() {
+//     button.classList.toggle("fas")
+//     button.classList.toggle("far")
+// })
+
+$(document).ready(function(){
+    $("#delete-modal").on("show.bs.modal", function(event){
+        //Get the button that triggered the modal
+        var button = $(event.relatedTarget);
+
+        // Extract value from the custom data-* attribute
+        var url = button.data("url");
+        $(this).find('#confirm-delete').attr('href', url)
+    });
+});
 
