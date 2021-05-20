62048e6ee44845bd7fb364273f41830a76f79718
keeks-mtl <wallarand@gmail.com>
2021-05-20 18:23:27 +0000

    Add explanations to code on script.js

--- static/js/script.js
+++ static/js/script.js
@@ -3,13 +3,16 @@ $(document).ready(function(){
         //Get the button that triggered the modal
         var button = $(event.relatedTarget);
 
-        // Extract value from the custom data-* attribute
+        // Extract value from the custom data-* attribute to link the plant_id 
         var url = button.data("url");
-        $(this).find('#confirm-delete').attr('href', url)
+        $(this).find('#confirm-delete').attr('href', url);
     });
     $( '.checkbox-validation' ).on('submit', function(e) {
+        // checks if any inputs for suitable_for are checked 
         if($('input[name="suitable_for"]:checked').length === 0) {
+            // prevents the submiting of the form
             e.preventDefault();
+            // adds an * to label so users know it is required
             $('.suitable-for').text("Suitable For *");
         }
     });
