034eea5d233d115872af3eb2166f27f594613584
keeks-mtl <wallarand@gmail.com>
2021-05-20 18:17:18 +0000

    Remove repeat function on script.js

--- static/js/script.js
+++ static/js/script.js
@@ -7,14 +7,6 @@ $(document).ready(function(){
         var url = button.data("url");
         $(this).find('#confirm-delete').attr('href', url)
     });
-    $("#profile-delete-modal").on("show.bs.modal", function(event){
-        //Get the button that triggered the modal
-        var button = $(event.relatedTarget);
-
-        // Extract value from the custom data-* attribute
-        var url = button.data("url");
-        $(this).find('#profile-confirm-delete').attr('href', url)
-    });
     $( '.checkbox-validation' ).on('submit', function(e) {
         if($('input[name="suitable_for"]:checked').length === 0) {
             e.preventDefault();
