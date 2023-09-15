e019497eb8989b4e7fcc64c146cabbccf2216aec
keeks-mtl <wallarand@gmail.com>
2023-09-15 11:21:33 +0100

    update spacing

--- app.py
+++ app.py
@@ -42,30 +42,21 @@ def created_by_is_user(username):
 @app.route("/plants")
 def plants():
     """
-    Gets all plants from the database.
-    Divides the number of plants in the database by the number of plants on the page.
-    Renders the plants page by plants and divides them into other pages.
+    Gets all plants from database.
+    Divides the number of plants in database by number of plants on page.
+    Renders plants page by plants and divides them into other pages.
     """
-    try:
-        plants_collection = mongo.db.plants
-        page = int(request.args.get('page') or 1)
-        # Number of plants per page
-        num = 12
-        # Finds how many pages there will be
-        count = ceil(float(plants_collection.count_documents({}) / num))
-        plants = list(plants_collection.find({}).sort("plant_like", -1).skip(
-            (page - 1) * num).limit(num))
-        return render_template(
-            "plants.html", plants=plants,
-            page=page, count=count, search=False)
-     except Exception as e:
-            # Handle the exception gracefully
-            print("Error while fetching data from MongoDB:", e)
-            retry_count -= 1  # Decrement the retry count
-            if retry_count > 0:
-                sleep(5)  # Sleep for 5 seconds before retrying
-            else:
-                return "Error: Maximum retry attempts reached"  # Return an error message after 5 retries
+    plants_collection = mongo.db.plants
+    page = int(request.args.get('page') or 1)
+    # number of plants per page
+    num = 12
+    # finds how many pages there will be
+    count = ceil(float(plants_collection.count_documents({}) / num))
+    plants = list(plants_collection.find({}).sort("plant_like", -1).skip(
+        (page - 1) * num).limit(num))
+    return render_template(
+        "plants.html", plants=plants,
+        page=page, count=count, search=False)
 
 
 # Register
